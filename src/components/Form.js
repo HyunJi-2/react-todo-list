dba16418cffd72163dbd2956c2e14e7d3c88db2b
HyunJi-2 <aaa9013@gmail.com>
2019-02-18 11:59:50 +0900

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Form.css';
+import './Form.scss';
 
 const Form = ({value, onChange, onCreate, onKeyPress, color }) => {
     return (
-    <div className="form">
-        <input value={value} onChange={onChange} onKeyPress={onKeyPress} style={{color}}/>
-        <div className="create-button" onClick={onCreate}>
-            추가
+        <div className="form">
+            <input value={value} onChange={onChange} onKeyPress={onKeyPress} style={{color}}/>
+            <div className="create-button" onClick={onCreate}>
+                추가
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Form;
